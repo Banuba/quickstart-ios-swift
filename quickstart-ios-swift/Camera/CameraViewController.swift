2aec514212954882b441416ee8e59b0f3101f7a6
ivangulidovbanuba <70957893+ivangulidovbanuba@users.noreply.github.com>
2020-09-08 16:40:28 +0300

    Update CameraViewController.swift
    added rotation

--- quickstart-ios-swift/Camera/CameraViewController.swift
+++ quickstart-ios-swift/Camera/CameraViewController.swift
@@ -3,25 +3,64 @@ import BanubaSdk
 
 class CameraViewController: UIViewController {
     
-    private var sdkManager = BanubaSdkManager()
     @IBOutlet weak var effectView: EffectPlayerView!
+    private var sdkManager = BanubaSdkManager()
+    private let config = EffectPlayerConfinguration(renderMode: .video)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         effectView.layoutIfNeeded()
-        sdkManager.setup(configuration: EffectPlayerConfinguration(renderMode: .video))
+        sdkManager.setup(configuration: config)
         effectView?.effectPlayer = sdkManager.effectPlayer
-        sdkManager.setRenderTarget(layer: effectView?.layer as! CAEAGLLayer, playerConfiguration: nil)
+        guard let effectView = self.effectView.layer as? CAEAGLLayer else { return }
+        sdkManager.setRenderTarget(layer: effectView, playerConfiguration: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        sdkManager.input.startCamera()
-        sdkManager.loadEffect("UnluckyWitch")
-        sdkManager.startEffectPlayer()
-    }
+       super.viewWillAppear(animated)
+       changeRenderSizeAndOrientation()
+       sdkManager.input.startCamera()
+       sdkManager.loadEffect("UnluckyWitch", synchronous: true)
+       sdkManager.startEffectPlayer()
+     }
     
     deinit {
         sdkManager.destroyEffectPlayer()
+    }
+    
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+       changeRenderSizeAndOrientation()
+    }
+    
+    private func setUpNewRenderTarget() {
+       sdkManager.stopEffectPlayer()
+       sdkManager.removeRenderTarget()
+       guard let effectView = self.effectView.layer as? CAEAGLLayer else { return }
+       sdkManager.setRenderTarget(layer: effectView, playerConfiguration: nil)
+       sdkManager.startEffectPlayer()
+    }
+    
+    private func changeRenderSizeAndOrientation() {
+       switch UIDevice.current.orientation {
+       case .portrait:
+           config.orientation = .deg0
+           config.renderSize = CGSize(width: 720, height: 1280)
+           setUpNewRenderTarget()
+       case .portraitUpsideDown:
+           config.orientation = .deg0
+           config.renderSize = CGSize(width: 720, height: 1280)
+           setUpNewRenderTarget()
+       case .landscapeLeft:
+           config.orientation = .deg270
+           config.renderSize = CGSize(width: 1280, height: 720)
+           setUpNewRenderTarget()
+       case .landscapeRight:
+           config.orientation = .deg90
+           config.renderSize = CGSize(width: 1280, height: 720)
+           setUpNewRenderTarget()
+       default:
+           break
+        }
     }
     
     @IBAction func closeCamera(_ sender: UIBarButtonItem) {
