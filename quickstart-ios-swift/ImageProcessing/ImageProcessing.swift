2c261b27cff6c5d088fd11a2d915e7470569c9b0
Alexei Latushko <alexei.latushko@banuba.com>
2022-05-24 11:53:50 +0400

    Add ImageProcessing View

---
+++ quickstart-ios-swift/ImageProcessing/ImageProcessing.swift
@@ -0,0 +1,70 @@
+import UIKit
+import BNBSdkApi
+
+class ProcessImageController: UIViewController {
+    
+    @IBOutlet weak var effectView: EffectPlayerView!
+    
+    private var sdkManager = BanubaSdkManager()
+    private let config = EffectPlayerConfiguration(renderMode: .video)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        effectView.layoutIfNeeded()
+        sdkManager.setup(configuration: config)
+        setUpRenderSize()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        sdkManager.input.startCamera()
+        _ = sdkManager.loadEffect("TrollGrandma", synchronous: true)
+        sdkManager.startEffectPlayer()
+    }
+    
+    deinit {
+        sdkManager.destroyEffectPlayer()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        sdkManager.stopEffectPlayer()
+        sdkManager.removeRenderTarget()
+        coordinator.animateAlongsideTransition(in: effectView, animation: { (UIViewControllerTransitionCoordinatorContext) in
+            self.sdkManager.autoRotationEnabled = true
+            self.setUpRenderSize()
+        }, completion: nil)
+    }
+    
+    private func setUpRenderTarget() {
+        sdkManager.setRenderTarget(view: effectView, playerConfiguration: nil)
+        sdkManager.startEffectPlayer()
+    }
+    
+    private func setUpRenderSize() {
+        switch UIApplication.shared.statusBarOrientation {
+        case .portrait:
+            config.orientation = .deg90
+            config.renderSize = CGSize(width: 720, height: 1280)
+            sdkManager.autoRotationEnabled = false
+            setUpRenderTarget()
+        case .portraitUpsideDown:
+            config.orientation = .deg270
+            config.renderSize = CGSize(width: 720, height: 1280)
+            setUpRenderTarget()
+        case .landscapeLeft:
+            config.orientation = .deg180
+            config.renderSize = CGSize(width: 1280, height: 720)
+            setUpRenderTarget()
+        case .landscapeRight:
+            config.orientation = .deg0
+            config.renderSize = CGSize(width: 1280, height: 720)
+            setUpRenderTarget()
+        default:
+            setUpRenderTarget()
+        }
+    }
+    
+    @IBAction func closeCamera(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
