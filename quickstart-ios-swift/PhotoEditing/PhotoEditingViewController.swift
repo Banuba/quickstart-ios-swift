3fb060084238e0d871e1fe3150bbbfc0a3643d77
Artem Shershen <artemshershen@Artems-MacBook-Pro.local>
2023-09-05 15:18:27 +0200

    fix UI

---
+++ quickstart-ios-swift/PhotoEditing/PhotoEditingViewController.swift
@@ -0,0 +1,83 @@
+import UIKit
+import Foundation
+import BNBSdkApi
+import BNBSdkCore
+
+class PhotoEditingViewController: UIViewController {
+    
+    @IBOutlet weak var effectView: EffectPlayerView!
+    @IBOutlet weak var selectImageButton: UIButton!
+    
+    private let player = Player()
+    private let photo = Photo()
+
+    private let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        effectView.layoutIfNeeded()
+        player.effectPlayer.effectManager()?.add(self)
+        player.use(input: photo, outputs: [effectView])
+        
+        let makeup = player.load(effect: "Makeup")
+        
+        // initialize Makeup effect with the Hair.color and the Eyes.color
+        makeup?.evalJs("Hair.color('1.0 0.0 0.0 0.5'); Eyes.color('1.0 0.0 0.0 0.5');", resultCallback: nil)
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        player.effectPlayer.effectManager()?.remove(self)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    @IBAction func selectImage(_ sender: UIButton) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func hairValueChanged(_ sender: UISlider) {
+        player.effect?.evalJs("Hair.color('1.0 \(sender.value / 10) 0.0 0.5')", resultCallback: nil)
+    }
+    
+    @IBAction func eyesValueChanged(_ sender: UISlider) {
+        player.effect?.evalJs("Eyes.color('1.0 \(sender.value / 10) 0.0 0.5')", resultCallback: nil)
+    }
+    
+    @IBAction func closeCamera(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoEditingViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.effectView.contentMode = .scaleAspectFit
+            self.photo.take(from: pickedImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension PhotoEditingViewController: BNBErrorListener {
+    func onError(_ domain: String, message: String) {
+        DispatchQueue.main.async { [weak self] in
+            self?.selectImageButton.isHidden = true
+            self?.dismiss(animated: true, completion: nil)
+            let alert = UIAlertController(
+                title: "Can't load the Makeup effect",
+                message: "This demo requires the Makeup effect in the 'effects' folder. You can find information about how to get the effect in README.",
+                preferredStyle: .alert
+            )
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self?.present(alert, animated: false, completion: nil)
+        }
+    }
+}
